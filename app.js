a9487f4c71a9572efbef31cef1be927a65330037
Ahmed ALHalwagy <ahmedhalwgy@gmail.com>
2023-05-16 04:08:55 +0300

    Prepared app for deployment

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const globalErrorHandler = require('./controller/errorController');
 
 const userRoute = require('./routes/userRoute');
 const medicineRoute = require('./routes/medicineRoute');
-// const medicineRoute = require('./routes/medicineRoute');
 const notesRoute = require('./routes/notesRoute');
+const compression = require('compression');
 
 //Start express app
 const app = express();
 
 app.use(express.json());
+
+app.use(compression());
 
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/medicines', medicineRoute);
